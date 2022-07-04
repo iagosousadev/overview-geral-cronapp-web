3bb515c927f9f5074dcbdff0bb85c214b1c9fc21
Francisco Iago De Sousa Mendes <francisco.iago@cronapp.io>
2022-07-04 17:46:08 +0000

    First commit...

---
+++ src/main/java/blockly/ClienteRest.java
@@ -0,0 +1,52 @@
+package blockly;
+
+import cronapi.*;
+import cronapi.rest.security.CronappSecurity;
+import java.util.Iterator;
+import java.util.concurrent.Callable;
+
+
+
+@CronapiMetaData(type = "blockly")
+@CronappSecurity(get = "Public", execute = "Public")
+public class ClienteRest {
+
+public static final int TIMEOUT = 300;
+
+/**
+ *
+ * @return Var
+ */
+// ClienteRest
+public static Var getCliente() throws Exception {
+ return new Callable<Var>() {
+
+   private Var response = Var.VAR_NULL;
+   private Var item = Var.VAR_NULL;
+   private Var i = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+
+    response =
+    cronapi.list.Operations.newList();
+
+    item =
+    cronapi.database.Operations.query(Var.valueOf("app.entity.Cliente"),Var.valueOf("select c.nome, c.cpf from Cliente c"));
+
+    for (Iterator it_i = item.iterator(); it_i.hasNext();) {
+        i = Var.valueOf(it_i.next());
+
+        cronapi.list.Operations.addLast(response,
+        cronapi.map.Operations.createObjectMapWith(Var.valueOf("nome",
+        cronapi.database.Operations.getField(i,
+        Var.valueOf("this[0]"))) , Var.valueOf("cpf",
+        cronapi.database.Operations.getField(i,
+        Var.valueOf("this[1]")))));
+    } // end for
+    return response;
+   }
+ }.call();
+}
+
+}
+
---
+++ src/main/webapp/js/blockly/Cliente.blockly.js
@@ -0,0 +1,18 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Cliente = window.blockly.js.blockly.Cliente || {};
+
+/**
+ * Descreva esta função...
+ */
+window.blockly.js.blockly.Cliente.autoCompletarEnderecoArgs = [];
+window.blockly.js.blockly.Cliente.autoCompletarEndereco = async function() {
+ var dadosCEP;
+  this.cronapi.util.getCEP(this.cronapi.screen.getValueOfField("Cliente.active.cep"), async function(sender_dadosCEP) {
+      dadosCEP = sender_dadosCEP;
+    this.cronapi.screen.changeValueOfField("Cliente.active.endereco", this.cronapi.json.getProperty(dadosCEP, 'logradouro'));
+    this.cronapi.screen.changeValueOfField("Cliente.active.cidade", this.cronapi.json.getProperty(dadosCEP, 'localidade'));
+    this.cronapi.screen.changeValueOfField("Cliente.active.uf", this.cronapi.json.getProperty(dadosCEP, 'uf'));
+  }.bind(this));
+}
